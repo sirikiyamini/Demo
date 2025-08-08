beea906996246a5217f05dd70a3a442ae489f1de
sirikiyamini <yaminisree2004@gmail.com>
2025-08-08 21:26:58 +0530

    xyz

---
+++ app.py
@@ -0,0 +1,21 @@
+from flask import Flask
+app=Flask(__name__)
+stores=[
+    {
+        "name":"My Store",
+        "items":[
+            {
+                "name":"Chair",
+                "price":15.99
+            }
+        ]
+    }
+]
+
+@app.get("/xyz")
+def get_stores():
+    return {"stores": stores}
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
