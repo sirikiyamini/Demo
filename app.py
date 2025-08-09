57ac9ef0fb9bcffafb422bc58b08ccdb9dfd85c7
sirikiyamini <yaminisree2004@gmail.com>
2025-08-09 19:22:04 +0530

    added few more methods

--- app.py
+++ app.py
@@ -23,6 +23,33 @@ def create_stores():
     stores.append(new_store)
     return new_store,201
 
+@app.post("/stores/<string:name>/item")
+def create_item(name):
+    request_data=request.get_json()
+    for store in stores:
+        if store["name"]==name:
+            new_item={"name":request_data["name"],"price":request_data["price"]}
+            store["items"].append(new_item)
+            return new_item,201
+    return {"message":"Store not found"} ,404  
+
+
+@app.get("/store/<string:name>")
+def get_store(name):
+    for store in stores:
+        if store["name"] == name:
+            return store
+    return {"message": "Store not found"}, 404 
+
+
+@app.get("/store/<string:name>/item")
+def get_item_in_store(name):
+    for store in stores:
+        if store["name"] == name:
+            return {"items": store["items"]}
+    return {"message": "Store not found"}, 404
+
+
 
 
 if __name__ == "__main__":
