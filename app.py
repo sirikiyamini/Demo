88d81dbd07f8c7e3a6cfc61997488f427b801511
sirikiyamini <yaminisree2004@gmail.com>
2025-08-09 17:11:28 +0530

    post method

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask,request
 app=Flask(__name__)
 stores=[
     {
@@ -15,6 +15,14 @@ stores=[
 @app.get("/stores")
 def get_stores():
     return {"stores": stores}
+
+@app.post("/stores")
+def create_stores():
+    request_data=request.get_json()
+    new_store={"name":request_data["name"],"items":[]}
+    stores.append(new_store)
+    return new_store,201
+
 
 
 if __name__ == "__main__":
