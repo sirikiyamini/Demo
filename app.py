1a573972c69e2133e924ac8fc8a75c747e3576ce
sirikiyamini <yaminisree2004@gmail.com>
2025-08-16 14:39:04 +0530

    new endpoints of api

--- app.py
+++ app.py
@@ -71,6 +71,40 @@ def get_item(item_id):
         return items[item_id]
     except KeyError:
         return {"message":"Item not found"},404
+    
+
+@app.delete("/item/<string:item_id>")
+def delete_item(item_id):
+    try:
+        del items[item_id]
+        return {"message": "Item deleted."}
+    except KeyError:
+        abort(404, message="Item not found.")
+
+
+@app.put("/item/<string:item_id>")
+def update_item(item_id):
+    item_data=request.get_json()
+    if "price" not in item_data or "name" not in item_data:
+        abort(400,message="Bad request. Ensure 'price' and 'name' are included in the JSON payload.")
+
+    try:
+        item=items[item_id]
+        item|=item_data
+        
+        return item
+    except KeyError:
+        abort(404,message="Item not found.")  
+
+
+@app.delete("/store/<string:store:id>")
+def delete_store(store_id):
+    try:
+        del stores[store_id]
+        return {"message":"Store deleted."}
+    except KeyError:
+        abort(404,message="Store not found.")      
+
 
 
 
