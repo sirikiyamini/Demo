6c6a7531126dc571feff1a06ffda515ed35cba46
sirikiyamini <yaminisree2004@gmail.com>
2025-08-16 11:36:19 +0530

    error handling

--- app.py
+++ app.py
@@ -1,16 +1,25 @@
 import uuid
 from flask import Flask,request
 from db import items,stores
+from flask_smorest import abort
 app=Flask(__name__)
 
 
-@app.get("/stores")
+@app.get("/store")
 def get_stores():
     return {"stores": list(stores.values())}
 
-@app.post("/stores")
+@app.post("/store")
 def create_stores():
     store_data=request.get_json()
+    if "name" not in store_data:
+        abort(
+            400,
+            message="Bad request. Ensure 'name' is included in the JSON payload.",
+        )
+    for store in stores.values():
+        if store_data["name"]==store["name"]:
+            abort(400,message=f"Store already exists.")    
     store_id=uuid.uuid4().hex
     store={**store_data,"id":store_id}
     stores[store_id]=store
@@ -19,9 +28,23 @@ def create_stores():
 @app.post("/item")
 def create_item():
     item_data=request.get_json()
+    if (
+        "price" not in item_data
+        or "store_id" not in item_data
+        or "name" not in item_data
+    ):
+        abort(
+            400,
+            message="Bad request. Ensure 'price', 'store_id', and 'name' are included in the JSON payload.",
+        )
+    for item in items.values():
+        if(
+            item_data["name"]==item["name"]
+            and item_data["store_id"]==item["store_id"]
+        ):
+            abort(400,message=f"Item already exists.")
     if item_data["store_id"] not in stores:
-        return {"message":"Store not found"},404
-    
+        abort(404,message="Store not found")
     item_id=uuid.uuid4().hex 
     item={**item_data,"id":item_id}
     items[item_id]=item
