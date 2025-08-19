f7631ef95fed094c9e260d23f155dcf791a39160
sirikiyamini <yaminisree2004@gmail.com>
2025-08-19 21:09:08 +0530

    flask-smorest

--- resources/item.py
+++ resources/item.py
@@ -4,12 +4,14 @@ from flask.views import MethodView
 from flask_smorest import Blueprint, abort
 
 from db import items
+from schemas import ItemSchema,ItemUpdateSchema
 
 blp = Blueprint("Items", __name__, description="Operations on items")
 
 
 @blp.route("/item/<string:item_id>")
 class Item(MethodView):
+    @blp.response(200,ItemSchema)
     def get(self, item_id):
         try:
             return items[item_id]
@@ -22,23 +24,15 @@ class Item(MethodView):
             return {"message": "Item deleted."}
         except KeyError:
             abort(404, message="Item not found.")
-
-    def put(self, item_id):
-        item_data = request.get_json()
-        # There's  more validation to do here!
-        # Like making sure price is a number, and also both items are optional
-        # Difficult to do with an if statement...
-        if "price" not in item_data or "name" not in item_data:
-            abort(
-                400,
-                message="Bad request. Ensure 'price', and 'name' are included in the JSON payload.",
-            )
+    
+    @blp.arguments(ItemUpdateSchema)
+    @blp.response(200,ItemSchema)
+    def put(self,item_data,item_id):
         try:
             item = items[item_id]
 
             # https://blog.teclado.com/python-dictionary-merge-update-operators/
             item |= item_data
-
             return item
         except KeyError:
             abort(404, message="Item not found.")
@@ -46,30 +40,20 @@ class Item(MethodView):
 
 @blp.route("/item")
 class ItemList(MethodView):
+    @blp.response(200,ItemSchema(many=True))
     def get(self):
-        return {"items": list(items.values())}
+        return items.values()
+    
 
-    def post(self):
-        item_data = request.get_json()
-        # Here not only we need to validate data exists,
-        # But also what type of data. Price should be a float,
-        # for example.
-        if (
-            "price" not in item_data
-            or "store_id" not in item_data
-            or "name" not in item_data
-        ):
-            abort(
-                400,
-                message="Bad request. Ensure 'price', 'store_id', and 'name' are included in the JSON payload.",
-            )
+    @blp.arguments(ItemSchema)
+    @blp.response(201,ItemSchema)
+    def post(self,item_data):
         for item in items.values():
-            if (
+            if(
                 item_data["name"] == item["name"]
                 and item_data["store_id"] == item["store_id"]
             ):
                 abort(400, message=f"Item already exists.")
-
         item_id = uuid.uuid4().hex
         item = {**item_data, "id": item_id}
         items[item_id] = item
--- resources/store.py
+++ resources/store.py
@@ -3,6 +3,7 @@ from flask import request
 from flask.views import MethodView
 from flask_smorest import Blueprint,abort
 from db import stores 
+from schemas import StoreSchema
 
 
 blp=Blueprint("stores",__name__,description="Operations on stores")
@@ -10,6 +11,7 @@ blp=Blueprint("stores",__name__,description="Operations on stores")
 
 @blp.route("/store/<string:store_id>")
 class Store(MethodView):
+    @blp.response(200,StoreSchema)
     def get(self,store_id):
         try:
             return stores[store_id]
@@ -26,19 +28,18 @@ class Store(MethodView):
 
 @blp.route("/store")
 class StoreList(MethodView):
+    @blp.response(200,StoreSchema(many=True))
     def get(self):
-        return {"stores": list(stores.values())}
+        return stores.values()
     
-    def post(self):
-        store_data=request.get_json()
-        if "name" not in store_data:
-            abort(
-                400,
-                message="Bad request. Ensure 'name' is included in the JSON payload."
-            )
+    @blp.arguments(StoreSchema)
+    @blp.response(201,StoreSchema)
+    def post(self,store_data):
         for store in stores.values():
             if store_data["name"]==store["name"]:
                 abort(400,message=f"Store already exists.")
+        
+
 
         store_id=uuid.uuid4().hex
         store={**store_data, "id":store_id}
