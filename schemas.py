7e6e0b37f542dc4f7c73be70e969dfc375691232
sirikiyamini <yaminisree2004@gmail.com>
2025-08-23 12:41:34 +0530

    many to many relationships

--- schemas.py
+++ schemas.py
@@ -17,13 +17,14 @@ class PlainStoreSchema(Schema):
     id=fields.Str(dump_only=True)
     name=fields.Str(required=True)
 
-class ItemSchema(PlainItemSchema):
-    store_id=fields.Int(required=True,load_only=True)
-    store=fields.Nested(PlainStoreSchema(),dump_only=True)
-
 class PlainTagSchema(Schema):
     id = fields.Int(dump_only=True)
     name = fields.Str()
+
+class ItemSchema(PlainItemSchema):
+    store_id=fields.Int(required=True,load_only=True)
+    store=fields.Nested(PlainStoreSchema(),dump_only=True)
+    tags = fields.List(fields.Nested(PlainTagSchema()), dump_only=True)
 
 class StoreSchema(PlainStoreSchema):
     items=fields.List(fields.Nested(PlainItemSchema()),dump_only=True)
@@ -32,6 +33,12 @@ class StoreSchema(PlainStoreSchema):
 class TagSchema(PlainTagSchema):
     store_id = fields.Int(load_only=True)
     store = fields.Nested(PlainStoreSchema(), dump_only=True)
+    items = fields.List(fields.Nested(PlainItemSchema()), dump_only=True)
+
+class TagAndItemSchema(Schema):
+    message = fields.Str()
+    item = fields.Nested(ItemSchema)
+    tag = fields.Nested(TagSchema)
      
 
 
