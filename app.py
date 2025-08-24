1f1b9b39bd5529f6af01ad0a029146402dd89c4d
sirikiyamini <yaminisree2004@gmail.com>
2025-08-24 10:53:13 +0530

    User authentication with Flask-JWT

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ from flask import Flask
 from flask_smorest import Api
 from flask_jwt_extended import JWTManager
 from flask import Flask, jsonify
+from blocklist import BLOCKLIST
 
 from db import db
 import models
@@ -32,6 +33,32 @@ def create_app(db_url=None):
 
     app.config["JWT_SECRET_KEY"] ="jose" #str(secrets.SystemRandom().getrandbits(128))
     jwt = JWTManager(app)
+    
+    @jwt.token_in_blocklist_loader
+    def check_if_token_in_blocklist(jwt_header, jwt_payload):
+        return jwt_payload["jti"] in BLOCKLIST
+
+
+    @jwt.revoked_token_loader
+    def revoked_token_callback(jwt_header, jwt_payload):
+        return (
+            jsonify(
+                {"description": "The token has been revoked.", "error": "token_revoked"}
+            ),
+            401,
+        )
+    
+    @jwt.needs_fresh_token_loader
+    def token_not_fresh_callback(jwt_header, jwt_payload):
+        return (
+            jsonify(
+                {
+                    "description": "The token is not fresh.",
+                    "error": "fresh_token_required",
+                }
+            ),
+            401,
+        )
 
     @jwt.additional_claims_loader
     def add_claims_to_jwt(identity):
--- resources/user.py
+++ resources/user.py
@@ -1,10 +1,18 @@
 from flask.views import MethodView
 from flask_smorest import Blueprint, abort
+from flask_jwt_extended import (
+    create_access_token,
+    get_jwt,
+    create_refresh_token,
+    get_jwt_identity,
+    jwt_required,
+)
 from passlib.hash import pbkdf2_sha256
-from flask_jwt_extended import create_access_token
+
 from db import db
 from models import UserModel
 from schemas import UserSchema
+from blocklist import BLOCKLIST
 
 
 blp = Blueprint("Users", "users", description="Operations on users")
@@ -14,6 +22,10 @@ class UserRegister(MethodView):
     @blp.arguments(UserSchema)
     def post(self, user_data):
         if UserModel.query.filter(UserModel.username == user_data["username"]).first():
+            access_token = create_access_token(identity=str(user.id), fresh=True)
+            refresh_token = create_refresh_token(user.id)
+            return {"access_token": access_token, "refresh_token": refresh_token}, 200
+        
             abort(409, message="A user with that username already exists.")
 
         user = UserModel(
@@ -38,6 +50,26 @@ class UserLogin(MethodView):
             return {"access_token": access_token}, 200
 
         abort(401, message="Invalid credentials.")
+
+@blp.route("/refresh")
+class TokenRefresh(MethodView):
+    @jwt_required(refresh=True)
+    def post(self):
+        current_user = get_jwt_identity()
+        new_token = create_access_token(identity=current_user, fresh=False)
+        # Make it clear that when to add the refresh token to the blocklist will depend on the app design
+        jti = get_jwt()["jti"]
+        BLOCKLIST.add(jti)
+        return {"access_token": new_token}, 200
+    
+
+@blp.route("/logout")
+class UserLogout(MethodView):
+    @jwt_required()
+    def post(self):
+        jti = get_jwt()["jti"]
+        BLOCKLIST.add(jti)
+        return {"message": "Successfully logged out"}, 200
 
 
 @blp.route("/user/<int:user_id>")
