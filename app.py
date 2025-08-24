5a27044483131b7ac4f4e0bf608ab47751832b9a
sirikiyamini <yaminisree2004@gmail.com>
2025-08-24 10:18:40 +0530

    Jwt claims and authorization

--- app.py
+++ app.py
@@ -1,12 +1,18 @@
 import os
+import secrets 
+
 from flask import Flask
 from flask_smorest import Api
+from flask_jwt_extended import JWTManager
+from flask import Flask, jsonify
+
 from db import db
 import models
 
 from resources.item import blp as ItemBlueprint
 from resources.store import blp as StoreBlueprint
 from resources.tag import blp as TagBlueprint
+from resources.user import blp as UserBlueprint
 
 
 def create_app(db_url=None):
@@ -24,12 +30,52 @@ def create_app(db_url=None):
     db.init_app(app)
     api = Api(app)
 
+    app.config["JWT_SECRET_KEY"] ="jose" #str(secrets.SystemRandom().getrandbits(128))
+    jwt = JWTManager(app)
+
+    @jwt.additional_claims_loader
+    def add_claims_to_jwt(identity):
+        if identity == 1:
+            return {"is_admin": True}
+        return {"is_admin": False}
+
+    @jwt.expired_token_loader
+    def expired_token_callback(jwt_header, jwt_payload):
+        return (
+            jsonify({"message": "The token has expired.", "error": "token_expired"}),
+            401,
+        )
+
+    @jwt.invalid_token_loader
+    def invalid_token_callback(error):
+        return (
+            jsonify(
+                {"message": "Signature verification failed.", "error": "invalid_token"}
+            ),
+            401,
+        )
+
+    @jwt.unauthorized_loader
+    def missing_token_callback(error):
+        return (
+            jsonify(
+                {
+                    "description": "Request does not contain an access token.",
+                    "error": "authorization_required",
+                }
+            ),
+            401,
+        )
+
+
+
     with app.app_context():
         db.create_all()
 
     api.register_blueprint(ItemBlueprint)
     api.register_blueprint(StoreBlueprint)
     api.register_blueprint(TagBlueprint)
+    api.register_blueprint(UserBlueprint)
     return app
 
 
